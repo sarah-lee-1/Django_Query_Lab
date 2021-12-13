df2290a2cf53485c3f2c4d638b989cada9403446
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-12-13 15:42:54 -0600

    Initial query code for Bonus One

--- querying_lab/school_db/views.py
+++ querying_lab/school_db/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Student, Instructor, Course, StudentCourse
-
+from django.db.models import Count
 
 def index(request):
     students = Student.objects.all()
@@ -88,8 +88,18 @@ def problem_six(request):
 
 # Create a view function and template for each bonus problem you complete
 
-# BONUS ONE
-# Write a query to find any instructors who are only teaching one single course. Display the instructor and the course
+BONUS ONE
+Write a query to find any instructors who are only teaching one single course. 
+Display the instructor and the course
+
+def BONUS ONE(request):
+    #instructor_one = Instructor.objects.filter('').order_by('')
+    course_one = Course.objects.alias(courses=Count('entry')).filter(courses__gt=1)
+    instructors_one = course_one.filter('instructor_id') 
+    context = {
+        'instructors': instructors_one
+    }
+    return render(request, 'school/two.html', context)
 
 # BONUS TWO
 # Display all students along with the number of credits they are taking
